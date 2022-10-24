86c00c6f3589a23e456d617e8a55d56f9418d801
JoseA4718 <joseaectec@estudiantec.cr>
2022-10-24 00:55:33 -0600

    Created all views and models
    API connection and local DB still needed

---
+++ MobileApplication/DetailTEC/DetailTEC/DetailTEC/Views/Points_Page.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace DetailTEC.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Points_Page : ContentPage
+    {
+        public Points_Page()
+        {
+            InitializeComponent();
+            updatePoints();
+        }
+
+        public void updatePoints()
+        {
+            Points.Text = Login_Page.CURRENTUSER.Points;
+            
+        }
+    }
+}
---
+++ MobileApplication/DetailTEC/DetailTEC/DetailTEC/Views/Profile_Page.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace DetailTEC.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Profile_Page : ContentPage
+    {
+        public Profile_Page()
+        {
+            InitializeComponent();
+        }
+    }
+}
