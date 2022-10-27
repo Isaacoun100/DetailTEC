8891bc9ccaee84fa0489e25b08cde3f353f834fa
Isaac Herrera Monge <Isaacoun100@gmail.com>
2022-10-27 10:41:46 -0600

    Manage Branches added

--- API/API/Models/Branch.cs
+++ API/API/Models/Branch.cs
@@ -4,6 +4,11 @@ public class Branch
 {
     public string nombre { get; set; } = string.Empty;
     public string fechaInicioGerente { get; set; } = string.Empty;
+    
+    //Fecha apertura was added, should be implemented in the API
+    public string fechaApertura { get; set; } = string.Empty;
+    
+    //Ubicacion should be splitted into 3 (provincia, canton, distrito) 
     public string ubicacion { get; set; } = string.Empty;
     public int gerente { get; set; }
     public string telefono { get; set; } = string.Empty;
