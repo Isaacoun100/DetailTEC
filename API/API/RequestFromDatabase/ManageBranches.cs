a49f292a579f3692b04973ce5fcc18727f0691fb
Isaac Herrera Monge <Isaacoun100@gmail.com>
2022-10-27 10:41:46 -0600

    Manage Branches added

--- API/API/RequestFromDatabase/ManageBranches.cs
+++ API/API/RequestFromDatabase/ManageBranches.cs
@@ -1,5 +1,21 @@
+using API.Models;
+using API.DataBaseQueries;
+
 namespace API.RequestFromDatabase; 
 
 public class ManageBranches {
+    
+    private DataBaseManager dataBaseManager= new DataBaseManager();
+
+    public Branch getBranch(string nombre) {
+
+
+        List<List<String>> data =  dataBaseManager.ReadOrderData(String.Format("SELECT * " +
+                                                                               "FROM Sucursal " +
+                                                                               "WHERE nombre='{0}'",nombre));
+        Branch branch = new Branch();
+
+        return branch;
+    }
     
 }
