3e98d4f0b318b7bc82365cdc773be6c61a2a44c0
Isaac Herrera Monge <Isaacoun100@gmail.com>
2022-10-28 21:39:37 -0600

    Connection Issues fixed

--- API/API/Models/Client.cs
+++ API/API/Models/Client.cs
@@ -5,7 +5,7 @@ public class Client
     public string nombreCompleto { get; set; } = string.Empty;
 
     public List<int> telefonos { get; set; } = new List<int>();
-    public string cedula { get; set; }
+    public string cedula { get; set; } = string.Empty;
     public string correo { get; set; }
     public string direccion { get; set; }
     public string usuario { get; set; } = string.Empty;
